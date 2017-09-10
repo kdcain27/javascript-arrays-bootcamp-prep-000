07ebfa3e527ea2ead0aff7d336fb85cae4e84802
Kevin Krause <kkrausesc@gmail.com>
2017-09-10 10:07:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,18 +1,3 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(array,element) {
-  array = [element, ...array]
-  return array;
-}
 
-function destructivelyAddElementToBeginningOfArray(array,element) {
-  return array.unshift(element);
-}
-
-function addElementToEndOfArray(array,element) {
-  return newArray[...array,"element"];
-}
-
-function destructivelyAddElementToEndOfArray(array,element) {
-  return array.push(element);
-}
